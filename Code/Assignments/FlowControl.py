fe15b3b418dcfaa34b8eaae8bdceb73e70f89752
kurinov.ilya@gmail.com <kurinov.ilya@gmail.com>
2020-01-05 22:32:13 +0200

    Update FlowControl.py

--- Code/Assignments/FlowControl.py
+++ Code/Assignments/FlowControl.py
@@ -21,10 +21,10 @@ if __name__  == "__main__":
     # Print welcomming words
 
 
-    #Get a number from user by input() function
+    # Get a number from user by input() function
 
 
-    # Convert the user's number to integer, as long as input() returns string
+    # Convert the user's number to integer (as long as input() returns string)
 
 
     # Generate random number using randint 
@@ -36,9 +36,9 @@ if __name__  == "__main__":
     
     # Check if user's guess is morte than computer's number
 
-        #Print to user that it is more
+        # Print to user that it is more
 
 
     # If it's not equal or more
 
-        #Print that it's too small
+        # Print that it's too small

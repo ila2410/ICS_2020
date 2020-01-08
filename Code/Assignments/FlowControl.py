baf77e2896f655e4bca8b9e429e23d85b2d4e375
Michael Vandagh <skywalker@Michaels-MacBook-Pro.local>
2020-01-09 01:08:36 +0200

    Revert "finished the first assignment on control flow"
    This reverts commit c548a1a2ab63bc646774f95e5a7537a274307019.

--- Code/Assignments/FlowControl.py
+++ Code/Assignments/FlowControl.py
@@ -14,11 +14,16 @@
 #########################################
 
 # Import random module
+<<<<<<< HEAD
 from random import randint
+=======
+
+>>>>>>> parent of c548a1a... finished the first assignment on control flow
 
 if __name__  == "__main__":
 
     # Print welcomming words
+<<<<<<< HEAD
     print('Hello!')
 
     # Get a number from user by input() function
@@ -44,3 +49,28 @@ if __name__  == "__main__":
     else:
         # Print that it's too small
         print('Number is too small')
+=======
+
+
+    # Get a number from user by input() function
+
+
+    # Convert the user's number to integer (as long as input() returns string)
+
+
+    # Generate random number using randint 
+
+    # Check if user's guess equal to computer's number
+
+        # Print to user that the guess is right
+
+    
+    # Check if user's guess is morte than computer's number
+
+        # Print to user that it is more
+
+
+    # If it's not equal or more
+
+        # Print that it's too small
+>>>>>>> parent of c548a1a... finished the first assignment on control flow

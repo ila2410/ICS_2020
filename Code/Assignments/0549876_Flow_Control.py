472b34412af1bb8ec467f0fcc689aced3a44c699
ila2410 <ila2410@users.noreply.github.com>
2020-01-08 13:08:41 +0200

    Create 0549876_Flow_Control.py

---
+++ Code/Assignments/0549876_Flow_Control.py
@@ -0,0 +1,44 @@
+
+# Let's make a game of guess a number game
+# At this game computer generates a number
+# and a user should guess it!
+
+#########################################
+#             Ingredients:              #
+#---------------------------------------#
+#       from random import randint      #
+#                 print()               #
+#                 input()               #
+#              if-elif-else             #
+#                  int()                #
+#########################################
+
+# Import random module
+
+
+if __name__  == "__main__":
+
+    # Print welcomming words
+
+
+    # Get a number from user by input() function
+
+
+    # Convert the user's number to integer (as long as input() returns string)
+
+
+    # Generate random number using randint 
+
+    # Check if user's guess equal to computer's number
+
+        # Print to user that the guess is right
+
+    
+    # Check if user's guess is morte than computer's number
+
+        # Print to user that it is more
+
+
+    # If it's not equal or more
+
+        # Print that it's too small

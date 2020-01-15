73ea6a2a9d8ceec549409aa2a171dd59a7482599
ila2410 <ila2410@users.noreply.github.com>
2020-01-15 07:16:31 +0200

    Added loops

---
+++ Code/Assignments/Return_Folder/Loops/Loops.py
@@ -0,0 +1,42 @@
+
+# Today's assignment is about 
+# Splitting students two into groups
+# 
+#=================================#
+#            Ingredients          #
+#---------------------------------#
+#     from random import choice   #
+#           list.remove()         #
+#               list()            #
+#               len()             #
+#                %                #
+#=================================#
+from random import choice
+
+# Here is your list of students
+# Your goal is to randomly split them into groups
+students = ['Andy', 'Michael', 'Jane', 'Jacob', 'Stan', 'Andrew', 'Max']
+
+# Create dictionary with groups 
+groups = {'Group A' : [], 'Group B' : []}
+
+# Use while loop to split students
+# Iterate till there are no students in the list
+#  You can check amount of students by len() function 
+
+
+    # if length of the list is mod of 2 (% operator)
+    # our group will be group A
+
+    # else group B
+
+    # Now it is time to choose a student
+    # Choose a student using choice() function
+
+
+    # Add chosen student to the group in the dictionary
+
+    # Remove student from the list  
+
+
+# Outside the loop print() your groups
